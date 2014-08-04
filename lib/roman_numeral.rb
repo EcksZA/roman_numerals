49bb9d56a49457f8454c3d7cb8fa644261789a21
Xolani Mfuko <xmfuko@gmail.com>
2014-08-03 17:21:09 -0700

    Solved for all numbers, except those with a 4 or 9 digit

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -2,15 +2,14 @@ def roman_numeral(number)
 	roman_types = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
 	holding_array = []
 
-	loop do
 		roman_types.each do |normal, roman|
-			if number >= normal
-				holding_array << roman
-				number -= normal
+			loop do
+				if number >= normal
+					holding_array << roman
+					number -= normal
+				end
+				break if number < normal
 			end
 		end
-		break if number == 0
-	end
 	return holding_array.join("")
-
 end
